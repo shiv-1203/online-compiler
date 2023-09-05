fd8c1addacac8551b4348b3d17e67ffd17a5b89c
shiv-1203 <105982373+shiv-1203@users.noreply.github.com>
2023-09-05 21:23:59 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ function App() {
 
     // Post request to compile endpoint
     console.log(userCode,userLang,userInput);
+    console.log("compile executed...");
     Axios.post(`https://online-compiler-z46n.onrender.com/api/postCode`, {
         code: userCode,
         language: userLang,
